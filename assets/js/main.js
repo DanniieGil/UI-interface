7d649724ce70d259df335098791b657a3a5822cf
DANIEL GIL <78768844+dannieowk@users.noreply.github.com>
2021-05-08 01:32:22 -0500

    v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
- 
+ // * Dinamics
 	$(document).ready(function(){
 		$('.menu-icon').click(function(){
 			$('.menu-icon,.menu,.main-box,footer,.logo,.subscribe').toggleClass('active')
 		})
 	})
+
++function ArrowMove(){
+	var arrow = document.getElementById("arrow-move")
+	$(arrow).click(function() {
+		$('html, body').animate({
+		scrollTop: $(".right").offset().top
+		}, 2000);
+	   });
+ }()
